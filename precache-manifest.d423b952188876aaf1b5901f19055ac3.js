e766f8ecd5f29e75b425f3b3048ddc338694c3da
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2019-02-23 17:51:21 +0100

    Updates

---
+++ precache-manifest.d423b952188876aaf1b5901f19055ac3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "50f7fd14ca63b8b05bdbf0554a5092d8",
+    "url": "/static/media/gb.50f7fd14.svg"
+  },
+  {
+    "revision": "4f1b7af5c0bae6aae85c3e7ba9401a85",
+    "url": "/static/media/gb.4f1b7af5.svg"
+  },
+  {
+    "revision": "f8952213641bba462c7314007909d394",
+    "url": "/static/media/fr.f8952213.svg"
+  },
+  {
+    "revision": "b1156355de9691d768df19a8a2b44da4",
+    "url": "/static/media/fr.b1156355.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1aebe4d08b143e2106f",
+    "url": "/static/js/main.b1aebe4d.chunk.js"
+  },
+  {
+    "revision": "c953c99095fd1b016193",
+    "url": "/static/js/1.c953c990.chunk.js"
+  },
+  {
+    "revision": "b1aebe4d08b143e2106f",
+    "url": "/static/css/main.c9a9557a.chunk.css"
+  },
+  {
+    "revision": "c953c99095fd1b016193",
+    "url": "/static/css/1.95cb93e4.chunk.css"
+  },
+  {
+    "revision": "e8e2fb874eb1d3aec8df506a2c4f7325",
+    "url": "/index.html"
+  }
+];
